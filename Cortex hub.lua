231631ec21e12f3837c940206fa1463f1150fb3f
Cortex <151866914+anilenzo@users.noreply.github.com>
2024-06-03 21:02:57 +0300

    Cortex hub.lua

--- Cortex hub.lua
+++ Cortex hub.lua
@@ -22,27 +22,32 @@ OrionLib:MakeNotification({
     Time = 3
 })
 
+local NoCooldownEnabled = false
+
 local Button = MainSection:AddButton({
     Name = "No cooldown",
     Callback = function()
-        local player = game.Players.LocalPlayer
-        local character = player.Character or player.CharacterAdded:Wait()
-        local tool = character:FindFirstChildOfClass("Tool") or player.Backpack:FindFirstChildOfClass("Tool")
+        NoCooldownEnabled = not NoCooldownEnabled
+        if NoCooldownEnabled then
+            local player = game.Players.LocalPlayer
+            local character = player.Character or player.CharacterAdded:Wait()
+            local tool = character:FindFirstChildOfClass("Tool") or player.Backpack:FindFirstChildOfClass("Tool")
 
-        if tool then
-            local localscript = tool:FindFirstChildOfClass("LocalScript")
-            if localscript then
-                while character.Humanoid.Health ~= 0 do
-                    local localscriptclone = localscript:Clone()
-                    localscript:Destroy()
-                    localscriptclone.Parent = tool
-                    wait(0.1)
+            if tool then
+                local localscript = tool:FindFirstChildOfClass("LocalScript")
+                if localscript then
+                    while character.Humanoid.Health ~= 0 and NoCooldownEnabled do
+                        local localscriptclone = localscript:Clone()
+                        localscript:Destroy()
+                        localscriptclone.Parent = tool
+                        wait(0.1)
+                    end
+                else
+                    warn("No LocalScript found in the tool.")
                 end
             else
-                warn("No LocalScript found in the tool.")
+                warn("No tool found in character or backpack.")
             end
-        else
-            warn("No tool found in character or backpack.")
         end
     end
 })
@@ -56,11 +61,14 @@ local RhythmSection = SpamTab:AddSection({
     Name = "Rhythm"
 })
 
+local RhythmExplosionEnabled = false
+
 local RhythmExplosionToggle = RhythmSection:AddToggle({
     Name = "Rhythm Explosion Spam",
     Default = false,
     Callback = function(Value)
-        while Value do
+        RhythmExplosionEnabled = Value
+        while RhythmExplosionEnabled do
             game:GetService("ReplicatedStorage").rhythmevent:FireServer("AoeExplosion", 0)
             wait()
         end
@@ -71,11 +79,14 @@ local RojoSection = SpamTab:AddSection({
     Name = "Rojo"
 })
 
+local RojoSpamEnabled = false
+
 local RojoSpamToggle = RojoSection:AddToggle({
     Name = "Rojo Spam",
     Default = false,
     Callback = function(Value)
-        while Value do
+        RojoSpamEnabled = Value
+        while RojoSpamEnabled do
             game:GetService("ReplicatedStorage"):WaitForChild("RojoAbility"):FireServer("Release", {game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame})
             wait()
         end
@@ -87,6 +98,7 @@ local NamedRojoSection = SpamTab:AddSection({
 })
 
 local Person = game.Players.LocalPlayer.Name
+
 local RojoUserTextbox = NamedRojoSection:AddTextbox({
     Name = "Make person use rojo spam",
     Default = "Username",
@@ -103,11 +115,14 @@ local NullSection = SpamTab:AddSection({
     Name = "Null"
 })
 
+local NullSpamEnabled = false
+
 local NullSpamToggle = NullSection:AddToggle({
     Name = "Null Spam",
     Default = false,
     Callback = function(Value)
-        while Value do
+        NullSpamEnabled = Value
+        while NullSpamEnabled do
             game:GetService("ReplicatedStorage").NullAbility:FireServer()
             wait()
         end
@@ -118,11 +133,14 @@ local RetroSection = SpamTab:AddSection({
     Name = "Retro"
 })
 
+local RetroSpamEnabled = false
+
 local RetroSpamToggle = RetroSection:AddToggle({
     Name = "Retro Spam (All gloves)",
     Default = false,
     Callback = function(Value)
-        while Value do
+        RetroSpamEnabled = Value
+        while RetroSpamEnabled do
             game:GetService("ReplicatedStorage").RetroAbility:FireServer(RetroAbility)
             wait()
         end
@@ -147,11 +165,14 @@ local SlapFarmSection = SlapFarmTab:AddSection({
     Name = "Slap farm"
 })
 
+local SlapFarmEnabled = false
+
 local SlapFarmToggle = SlapFarmSection:AddToggle({
     Name = "Slap farm",
     Default = false,
     Callback = function(Value)
-        if Value then
+        SlapFarmEnabled = Value
+        if SlapFarmEnabled then
             print("Slap farm started")
             startSlapFarm()
         else
@@ -162,85 +183,89 @@ local SlapFarmToggle = SlapFarmSection:AddToggle({
 })
 
 function startSlapFarm()
-    ToggleLoop = true
-    while ToggleLoop do
+    while SlapFarmEnabled do
         loadstring(game:HttpGet('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Slap%20Farm'))()
         wait(1)
     end
 end
 
 function stopSlapFarm()
-    ToggleLoop = false
+    SlapFarmEnabled = false
 end
 
-local SlapTab = Window:CreateTab("Slap Aura", nil)
-local SlapSection = SlapTab:CreateSection("Slap Aura Section")
+local SlapAuraTab = Window:MakeTab({
+    Name = "Slap Aura",
+    PremiumOnly = false
+})
 
-local SlapAuraButton = SlapSection:CreateButton({
-    Name = "Activate Slap Aura",
-    Callback = function()
-        isSlapAuraActive = not isSlapAuraActive
+local SlapAuraSection = SlapAuraTab:AddSection({
+    Name = "Slap Aura"
+})
+
+local isSlapAuraActive = false
+
+local SlapAuraToggle = SlapAuraSection:AddToggle({
+    Name = "Slap Aura",
+    Default = false,
+    Callback = function(Value)
+        isSlapAuraActive = Value
         if isSlapAuraActive then
-            print("Slap Aura activated")
             startSlapAura()
         else
-            print("Slap Aura deactivated")
             stopSlapAura()
         end
     end,
 })
 
-local TeleportsTab = Window:CreateTab("Teleports", nil)
-
-local TeleportsSection = TeleportsTab:CreateSection("Teleport Locations")
-
-local TeleportDropdown = TeleportsSection:CreateDropdown({
-    Name = "Teleport",
-    Default = "",
-    Options = {"Safe spot", "Arena", "Default Arena", "Lobby", "Tournament", "Moai Island", "Slapple Island", "Plate"},
-    Callback = function(Value)
-        if Value == "Safe spot" then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Spot.CFrame * CFrame.new(0, 28, 0)
-        elseif Value == "Arena" then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.Origo.CFrame * CFrame.new(0, -5, 0)
-        elseif Value == "Moai Island" then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-61.02, 38, 744.37)
-        elseif Value == "Slapple Island" then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3000, 150, 350)
-        elseif Value == "Plate" then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.ElevatorPlate.CFrame * CFrame.new(0, 8, 0)
-        elseif Value == "Lobby" then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-35, 2, 4)
-        elseif Value == "Tournament" then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(21, 3, 140)
-        elseif Value == "Default Arena" then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.DefaultSpawn.CFrame
+function startSlapAura()
+    isSlapAuraActive = true
+    while isSlapAuraActive do
+        for _, player in pairs(game.Players:GetPlayers()) do
+            if player.Character and player.Character:FindFirstChild("HumanoidRootPart") and player ~= game.Players.LocalPlayer and not player.Character.Head:FindFirstChild("UnoReverseCard") then
+                if (player.Character.HumanoidRootPart.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 50 then
+                    game:GetService("ReplicatedStorage").b:FireServer(player.Character["Right Arm"])
+                    wait(0.1)
+                end
+            end
         end
-    end    
+        wait(0.1)
+    end
+end
+
+function stopSlapAura()
+    isSlapAuraActive = false
+end
+
+local TeleportsTab = Window:MakeTab({
+    Name = "Teleports",
+    PremiumOnly = false
 })
 
-local AntiLava = nil
-local AntiAcid = nil
+local TeleportsSection = TeleportsTab:AddSection({
+    Name = "Teleport Locations"
+})
 
-local SlapRoyaleTab = Window:CreateTab("Slap Royale", nil) -- "Slap Royale" adında bir tab oluşturuluyor
+local SlapRoyaleTab = Window:MakeTab({
+    Name = "Slap Royale",
+    PremiumOnly = false
+})
 
-local SlapRoyaleSection = SlapRoyaleTab:CreateSection("Anti acid/Anti lava") -- "Slap Royale" tabında "Anti acid/Anti lava" adında bir bölüm oluşturuluyor
+local SlapRoyaleSection = SlapRoyaleTab:AddSection({
+    Name = "Anti acid/Anti lava"
+})
 
-local ToggleAntiAcidLava = SlapRoyaleSection:CreateToggle({
+local ToggleAntiAcidLava = SlapRoyaleSection:AddToggle({
     Name = "Toggle Anti acid/Anti lava",
     CurrentValue = false,
-    Flag = "ToggleAntiAcidLava", 
+    Flag = "ToggleAntiAcidLava",
     Callback = function(Value)
         if Value then
-            -- Önceki AntiLava ve AntiAcid parçalarını yok et
             if AntiLava then
                 AntiLava:Destroy()
             end
             if AntiAcid then
                 AntiAcid:Destroy()
             end
-
-            -- Yeni AntiLava parçasını oluştur
             AntiLava = Instance.new("Part")
             AntiLava.Name = "AntiLava"
             AntiLava.Position = Vector3.new(-238, -43, 401)
@@ -250,7 +275,6 @@ local ToggleAntiAcidLava = SlapRoyaleSection:CreateToggle({
             AntiLava.CanCollide = false
             AntiLava.Parent = workspace
 
-            -- Yeni AntiAcid parçasını oluştur
             AntiAcid = Instance.new("Part")
             AntiAcid.Name = "AntiAcid"
             AntiAcid.Position = Vector3.new(-70, -20, -725)
@@ -260,7 +284,6 @@ local ToggleAntiAcidLava = SlapRoyaleSection:CreateToggle({
             AntiAcid.CanCollide = false
             AntiAcid.Parent = workspace
         else
-            -- Anahtarlama düğmesi kapatıldığında, AntiLava ve AntiAcid parçalarını yok et
             if AntiLava then
                 AntiLava:Destroy()
                 AntiLava = nil
@@ -273,11 +296,16 @@ local ToggleAntiAcidLava = SlapRoyaleSection:CreateToggle({
     end,
 })
 
-local BadgeTab = Window:CreateTab("Badge teleport", nil)
+local BadgeTab = Window:MakeTab({
+    Name = "Badge teleport",
+    PremiumOnly = false
+})
 
-local BadgeSection = BadgeTab:CreateSection("Elude/Counter")
+local BadgeSection = BadgeTab:AddSection({
+    Name = "Elude/Counter"
+})
 
-BadgeSection:CreateButton({
+BadgeSection:AddButton({
     Name = "Teleport",
     Callback = function()
         game:GetService("TeleportService"):Teleport(11828384869)
