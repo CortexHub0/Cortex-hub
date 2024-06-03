113be66e399ced34a4a43cc2907a2fc6ee77670a
Cortex <151866914+anilenzo@users.noreply.github.com>
2024-06-03 18:24:28 +0300

    Cortex hub

--- Cortex hub.lua
+++ Cortex hub.lua
@@ -1,32 +1,15 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-
 local Window = Rayfield:CreateWindow({
-    Name = "Cortex Hub",
+    Name = "Cortex hub",
     LoadingTitle = "Corhub",
     LoadingSubtitle = "by Cortex",
-    ConfigurationSaving = {
-       Enabled = true,
-       FolderName = nil, -- Create a custom folder for your hub/game
-       FileName = "Cortex Hub"
-    },
     Discord = {
        Enabled = true,
-       Invite = "Cortex Exploits", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-    },
-    KeySystem = false, -- Set this to true to use our key system
-    KeySettings = {
-       Title = "Untitled",
-       Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided",
-       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+       Invite = "Cortex Exploits",
+       RememberJoins = true
     }
 })
 
-local MainTab = Window:CreateTab("Home", nil) -- Title, Image
+local MainTab = Window:CreateTab("Ana Sayfa", nil)
 local MainSection = MainTab:CreateSection("Main")
 
 Rayfield:Notify({
