97a9a833ac21cdc15dbd46e6ec2105c93d3e79e5
Cortex <151866914+anilenzo@users.noreply.github.com>
2024-06-09 19:15:01 +0300

    Update Corlib.lua

--- Corlib.lua
+++ Corlib.lua
@@ -6,9 +6,9 @@ local Window = OrionLib:MakeWindow({Name = "MurchCase and Relic ESP and AutoColl
 
 -- Sekme oluşturun
 local Tab = Window:MakeTab({
-	Name = "Main",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+    Name = "Main",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
 -- ESP işlevi
@@ -24,6 +24,8 @@ local function enableESP(item)
     Box.ZIndex = 1
     Box.AlwaysOnTop = true
     Box.Parent = item
+    
+    print("ESP enabled for:", item.Name)
 end
 
 -- AutoCollect işlevi
@@ -32,12 +34,16 @@ local function autoCollect(item)
     wait(0.5)
     firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, item, 0)
     firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, item, 1)
+    
+    print("AutoCollected:", item.Name)
 end
 
 -- ESP ve AutoCollect için kontrol işlevi
 local function checkItems(name)
+    local found = false
     for _, item in pairs(workspace:GetDescendants()) do
         if item.Name == name and item:IsA("BasePart") then
+            found = true
             if espEnabled then
                 enableESP(item)
             end
@@ -46,60 +52,63 @@ local function checkItems(name)
             end
         end
     end
+    if not found then
+        print(name, "not found in workspace")
+    end
 end
 
 -- MurchCase ESP
 local espEnabled = false
 Tab:AddToggle({
-	Name = "MurchCase ESP",
-	Default = false,
-	Callback = function(Value)
-		espEnabled = Value
-		while espEnabled do
-			checkItems("MurchCase")
-			wait(1)
-		end
-	end    
+    Name = "MurchCase ESP",
+    Default = false,
+    Callback = function(Value)
+        espEnabled = Value
+        while espEnabled do
+            checkItems("MurchCase")
+            wait(1)
+        end
+    end    
 })
 
 -- MurchCase AutoCollect
 local autoCollectEnabled = false
 Tab:AddToggle({
-	Name = "MurchCase AutoCollect",
-	Default = false,
-	Callback = function(Value)
-		autoCollectEnabled = Value
-		while autoCollectEnabled do
-			checkItems("MurchCase")
-			wait(1)
-		end
-	end    
+    Name = "MurchCase AutoCollect",
+    Default = false,
+    Callback = function(Value)
+        autoCollectEnabled = Value
+        while autoCollectEnabled do
+            checkItems("MurchCase")
+            wait(1)
+        end
+    end    
 })
 
 -- Relic ESP
 Tab:AddToggle({
-	Name = "Relic ESP",
-	Default = false,
-	Callback = function(Value)
-		espEnabled = Value
-		while espEnabled do
-			checkItems("Relic")
-			wait(1)
-		end
-	end    
+    Name = "Relic ESP",
+    Default = false,
+    Callback = function(Value)
+        espEnabled = Value
+        while espEnabled do
+            checkItems("Relic")
+            wait(1)
+        end
+    end    
 })
 
 -- Relic AutoCollect
 Tab:AddToggle({
-	Name = "Relic AutoCollect",
-	Default = false,
-	Callback = function(Value)
-		autoCollectEnabled = Value
-		while autoCollectEnabled do
-			checkItems("Relic")
-			wait(1)
-		end
-	end    
+    Name = "Relic AutoCollect",
+    Default = false,
+    Callback = function(Value)
+        autoCollectEnabled = Value
+        while autoCollectEnabled do
+            checkItems("Relic")
+            wait(1)
+        end
+    end    
 })
 
 -- Kullanıcı arayüzünü başlatın
